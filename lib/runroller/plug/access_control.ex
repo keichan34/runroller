a8adc4c4d7bc5580130b622b5429af28d93c3321
Keita Kobayashi <keita@kkob.us>
2015-09-19 12:46:53 +0900

    Correctly respond to HTTP OPTIONS

--- lib/runroller/plug/access_control.ex
+++ lib/runroller/plug/access_control.ex
@@ -1,10 +1,22 @@
 defmodule Runroller.Plug.AccessControl do
   @behaviour Plug
 
-  import Plug.Conn, only: [put_resp_header: 3]
+  alias Plug.Conn
+  import Plug.Conn #, only: [put_resp_header: 3, put_status: 2, halt: 1]
 
   def init([]), do: []
+  def call(%Conn{method: "OPTIONS"} = conn, []) do
+    conn
+    |> put_cors_headers
+    |> send_resp(200, "")
+    |> halt
+  end
   def call(conn, []) do
+    conn
+    |> put_cors_headers
+  end
+
+  defp put_cors_headers(conn) do
     conn
     |> put_resp_header("access-control-allow-origin", "*")
     |> put_resp_header("access-control-max-age", "1728000") # 20 days
