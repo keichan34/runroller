226ba486138c223100eb52a4d9704a65f9c7fe50
Keita Kobayashi <keita@kkob.us>
2015-09-22 20:29:01 +0900

    Better logging of backend requests

--- lib/runroller/query/httpoison_adapter.ex
+++ lib/runroller/query/httpoison_adapter.ex
@@ -1,15 +1,21 @@
 defmodule Runroller.Query.HTTPoisonAdapter do
   @behaviour Runroller.Query
 
+  require Logger
+
   def head(uri, headers) do
+    Logger.info "URI=#{uri} ; Start HEAD"
     case HTTPoison.head(uri, headers) do
       {:ok, %HTTPoison.Response{} = resp} ->
         headers = resp.headers
         |> Enum.map(fn({k, v}) -> {String.downcase(k), v} end)
         |> Enum.into(%{})
 
+        Logger.info "URI=#{uri} ; Received #{resp.status_code}"
+
         {:ok, resp.status_code, headers}
       {:error, error} ->
+        Logger.info "URI=#{uri} ; Error: #{error.reason}"
         {:error, error.reason}
     end
   end
